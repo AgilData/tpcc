2908b246a42061fbd6cc14e17419f27e8a4a5801
Andy Grove <andygrove73@gmail.com>
2016-09-19 16:13:51 -0600

    change SQL for zero

--- src/main/java/com/codefutures/tpcc/TpccStatements.java
+++ src/main/java/com/codefutures/tpcc/TpccStatements.java
@@ -22,7 +22,7 @@ public class TpccStatements {
         this.conn = conn;
 
         // NewOrder statements.
-        pStmts[0]  = prepareStatement("SELECT c_discount, c_last, c_credit, w_tax FROM customer, warehouse WHERE w_id = ? AND c_w_id = ? AND c_d_id = ? AND c_id = ?");
+        pStmts[0]  = prepareStatement("SELECT c.c_discount, c.c_last, c.c_credit, w.w_tax FROM customer c JOIN warehouse w ON c.c_w_id = w_id AND w.w_id = ? AND c.c_w_id = ? AND c.c_d_id = ? AND c.c_id = ?");
         pStmts[1]  = prepareStatement("SELECT d_next_o_id, d_tax FROM district WHERE d_id = ? AND d_w_id = ? FOR UPDATE");
         pStmts[2]  = prepareStatement("UPDATE district SET d_next_o_id = ? + 1 WHERE d_id = ? AND d_w_id = ?");
         pStmts[3]  = prepareStatement("INSERT INTO orders (o_id, o_d_id, o_w_id, o_c_id, o_entry_d, o_ol_cnt, o_all_local) VALUES(?, ?, ?, ?, ?, ?, ?)");
@@ -64,7 +64,7 @@ public class TpccStatements {
         // Slev statements.
         pStmts[32] = prepareStatement("SELECT d_next_o_id FROM district WHERE d_id = ? AND d_w_id = ?");
         pStmts[33] = prepareStatement("SELECT DISTINCT ol_i_id FROM order_line WHERE ol_w_id = ? AND ol_d_id = ? AND ol_o_id < ? AND ol_o_id >= (? - 20)");
-        pStmts[34] = prepareStatement("SELECT count(*) FROM stock WHERE s_w_id = ? AND s_i_id = ? AND s_quantity < ?");
+        pStmts[34] = prepareStatement("SELECT count(s_w_id) FROM stock WHERE s_w_id = ? AND s_i_id = ? AND s_quantity < ?");
 
         // These are used in place of pStmts[0] in order to avoid joins
         pStmts[35]  = prepareStatement("SELECT c_discount, c_last, c_credit FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?");
